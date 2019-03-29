d7b0f85d194a5c6d3a6a407e62b6f4dc990cd837
charlie83Gs <carlosgomezsoza@gmail.com>
2019-03-28 18:32:05 -0600

    first commit

---
+++ public/javascripts/tarea.js
@@ -0,0 +1,26 @@
+function setup(){
+	createCanvas(600,600);
+
+
+}
+
+function draw(){
+	background(0);
+
+}
+
+const Direction = {
+	UP:0,
+	DOWN:1,
+	LEFT:2,
+	RIGHT:3,
+
+}
+
+class Figure{
+	contructor(){
+
+
+	}
+
+}
